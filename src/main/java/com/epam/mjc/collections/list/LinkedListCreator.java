4670629c38f7f630f80afc179244746dc49cc0e9
kirichenkods <kirichenkods@ya.ru>
2024-04-17 23:47:21 +0300

    add solution

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,16 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (Integer item : sourceList) {
+           if (item % 2 != 0) {
+               linkedList.addFirst(item);
+           } else {
+               linkedList.addLast(item);
+           }
+        }
+
+        return linkedList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,27 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator listComparator = new ListComparator();
+        sourceList.sort(listComparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int intA = Integer.parseInt(a);
+        int intB = Integer.parseInt(b);
+        int calcA = calc(intA);
+        int calcB = calc(intB);
+
+        if (calcA != calcB) {
+            return Integer.compare(calcA, calcB);
+        }
+
+        return Integer.compare(intA, intB);
+    }
+
+    private int calc(int x) {
+        return 5 * x * x + 3;
     }
 }
